684227fe389092e89cb10c150c2b77e61996527c
rexim <reximkut@gmail.com>
2023-04-25 07:23:36 +0700

    Introduce js/ and ts/ folders

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
-type Cell = number;
-
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 class Board {
-    public width: number;
-    public height: number;
-    public cells: Cell[];
-
-    constructor(width: number, height: number, cell: number = 0) {
+    constructor(width, height, cell = 0) {
         this.width = width;
         this.height = height;
-        this.cells = Array(width*height).fill(cell);
+        this.cells = Array(width * height).fill(cell);
     }
-
-    get(x: number, y: number): Cell {
-        return this.cells[y*this.width + x];
+    get(x, y) {
+        return this.cells[y * this.width + x];
     }
-
-    set(x: number, y: number, cell: Cell) {
-        return this.cells[y*this.width + x] = cell;
+    set(x, y, cell) {
+        return this.cells[y * this.width + x] = cell;
     }
 }
-
-function mod(a: number, b: number): number {
-    return (a%b + b)%b
+function mod(a, b) {
+    return (a % b + b) % b;
 }
-
-function countNbors(board: Board, nbors: number[], x0: number, y0: number) {
+function countNbors(board, nbors, x0, y0) {
     nbors.fill(0);
     for (let dy = -1; dy <= 1; ++dy) {
         for (let dx = -1; dx <= 1; ++dx) {
@@ -36,18 +36,7 @@ function countNbors(board: Board, nbors: number[], x0: number, y0: number) {
         }
     }
 }
-
-interface State {
-    "color": string;
-    "default": number;
-    "transitions": {
-        [key: string]: number;
-    }
-}
-
-type Automaton = State[];
-
-const Seeds: Automaton = [
+const Seeds = [
     {
         "transitions": {
             "62": 1,
@@ -61,8 +50,7 @@ const Seeds: Automaton = [
         "color": "#FF5050",
     },
 ];
-
-const GoL: Automaton = [
+const GoL = [
     {
         "transitions": {
             "53": 1,
@@ -79,8 +67,7 @@ const GoL: Automaton = [
         "color": "#FF5050",
     },
 ];
-
-const BB: Automaton = [
+const BB = [
     // 0 - Dead
     {
         "transitions": {
@@ -108,8 +95,7 @@ const BB: Automaton = [
         "color": "#50FF50",
     }
 ];
-
-function computeNextBoard(automaton: Automaton, current: Board, next: Board) {
+function computeNextBoard(automaton, current, next) {
     console.assert(current.width == next.width);
     console.assert(current.height == next.height);
     const nbors = new Array(automaton.length).fill(0);
@@ -124,46 +110,40 @@ function computeNextBoard(automaton: Automaton, current: Board, next: Board) {
         }
     }
 }
-
-function render(ctx: CanvasRenderingContext2D, automaton: Automaton, board: Board) {
-    const CELL_WIDTH = ctx.canvas.width/board.width;
-    const CELL_HEIGHT = ctx.canvas.height/board.height;
-
+function render(ctx, automaton, board) {
+    const CELL_WIDTH = ctx.canvas.width / board.width;
+    const CELL_HEIGHT = ctx.canvas.height / board.height;
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     for (let y = 0; y < board.height; ++y) {
         for (let x = 0; x < board.width; ++x) {
-            const rx = x*CELL_WIDTH;
-            const ry = y*CELL_HEIGHT;
+            const rx = x * CELL_WIDTH;
+            const ry = y * CELL_HEIGHT;
             ctx.fillStyle = automaton[board.get(x, y)].color;
             ctx.fillRect(rx, ry, CELL_WIDTH, CELL_HEIGHT);
         }
     }
 }
-
-function bytesAsHexString(bytes: Uint8Array): string {
+function bytesAsHexString(bytes) {
     let result = "";
     for (let i = 0; i < bytes.length; ++i) {
         result += bytes[i].toString(16).padStart(2, '0').toUpperCase();
     }
     return result;
 }
-
-function transcendentalApprehensionOfImage(image: ImageData, rx: number, ry: number, rw: number, rh: number): [Board, Automaton] {
+function transcendentalApprehensionOfImage(image, rx, ry, rw, rh) {
     const board = new Board(rw, rh);
     let count = 0;
-    const colorToState: { [key: string]: number } = {};
+    const colorToState = {};
     for (let y = ry; y < ry + rh; ++y) {
         for (let x = rx; x < rx + rw; ++x) {
-            const pixel = new Uint8Array(image.data.buffer, (y*image.width + x)*4, 4);
+            const pixel = new Uint8Array(image.data.buffer, (y * image.width + x) * 4, 4);
             const color = bytesAsHexString(pixel);
             if (colorToState[color] === undefined) {
                 colorToState[color] = count++;
             }
-
             board.set(x - rx, y - ry, colorToState[color]);
         }
     }
-
     const automaton = Object.keys(colorToState).map((color) => {
         return {
             "color": `#${color}`,
@@ -171,17 +151,13 @@ function transcendentalApprehensionOfImage(image: ImageData, rx: number, ry: num
             "transitions": {},
         };
     });
-
     return [board, automaton];
 }
-
-window.onload = async () => {
-    const cute = await stbi_load_from_url("img/Cute People Icon v2.png");
-    const [cuteBoard, cuteAutomaton] = transcendentalApprehensionOfImage(cute, 0, 0, cute.width/4, cute.height/3);
-
+window.onload = () => __awaiter(void 0, void 0, void 0, function* () {
+    const cute = yield stbi_load_from_url("img/Cute People Icon v2.png");
+    const [cuteBoard, cuteAutomaton] = transcendentalApprehensionOfImage(cute, 0, 0, cute.width / 4, cute.height / 3);
     console.log(cuteBoard);
     console.log(cuteAutomaton);
-
     const nbors = new Array(cuteAutomaton.length).fill(0);
     for (let y = 0; y < cuteBoard.height; ++y) {
         for (let x = 0; x < cuteBoard.width; ++x) {
@@ -193,11 +169,9 @@ window.onload = async () => {
             }
         }
     }
-
     console.log(cuteAutomaton);
-
     const canvasId = "app";
-    const app = document.getElementById(canvasId) as HTMLCanvasElement;
+    const app = document.getElementById(canvasId);
     if (app === null) {
         throw new Error(`Could not find canvas ${canvasId}`);
     }
@@ -206,67 +180,55 @@ window.onload = async () => {
     if (ctx === null) {
         throw new Error(`Could not initialize 2d context`);
     }
-
     const nextId = "next";
-    const next = document.getElementById(nextId) as HTMLButtonElement;
+    const next = document.getElementById(nextId);
     if (next == null) {
         throw new Error(`Could not find button ${nextId}`);
     }
-
     const playId = "play";
-    const play = document.getElementById(playId) as HTMLButtonElement;
+    const play = document.getElementById(playId);
     if (play == null) {
         throw new Error(`Could not find button ${playId}`);
     }
-
     let currentAutomaton = cuteAutomaton;
-    let currentBoard: Board = cuteBoard;
-    let nextBoard: Board = new Board(currentBoard.width, currentBoard.height);
-
-    app.height = app.width*(currentBoard.height/currentBoard.width);
-    
+    let currentBoard = cuteBoard;
+    let nextBoard = new Board(currentBoard.width, currentBoard.height);
+    app.height = app.width * (currentBoard.height / currentBoard.width);
     app.addEventListener("click", (e) => {
-        const CELL_WIDTH = app.width/currentBoard.width;
-        const CELL_HEIGHT = app.height/currentBoard.height;
-        
-        const x = Math.floor(e.offsetX/CELL_WIDTH);
-        const y = Math.floor(e.offsetY/CELL_HEIGHT);
-
+        const CELL_WIDTH = app.width / currentBoard.width;
+        const CELL_HEIGHT = app.height / currentBoard.height;
+        const x = Math.floor(e.offsetX / CELL_WIDTH);
+        const y = Math.floor(e.offsetY / CELL_HEIGHT);
         const state = document.getElementsByName("state");
         for (let i = 0; i < state.length; ++i) {
-            if ((state[i] as HTMLInputElement).checked) {
+            if (state[i].checked) {
                 currentBoard.set(x, y, i);
                 render(ctx, currentAutomaton, currentBoard);
                 return;
             }
         }
     });
-
     const nextState = () => {
         computeNextBoard(currentAutomaton, currentBoard, nextBoard);
         [currentBoard, nextBoard] = [nextBoard, currentBoard];
         render(ctx, currentAutomaton, currentBoard);
     };
-
     next.addEventListener("click", nextState);
-
     const PLAY_PERIOD = 100;
-    let playInterval: number | null = setInterval(nextState, PLAY_PERIOD);
-
+    let playInterval = setInterval(nextState, PLAY_PERIOD);
     play.addEventListener("click", () => {
         if (playInterval === null) {
             playInterval = setInterval(nextState, PLAY_PERIOD);
             play.innerText = "Pause";
-        } else {
+        }
+        else {
             clearInterval(playInterval);
             playInterval = null;
             play.innerText = "Play";
         }
-    })
-
-
+    });
     render(ctx, currentAutomaton, currentBoard);
-};
-
+});
 // TODO: drawing the cells
 // TODO: autopopulate radio buttons based on the current automaton
+//# sourceMappingURL=index.js.map
--- ts/index.ts
+++ ts/index.ts
@@ -1,30 +1,30 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
+type Cell = number;
+
 class Board {
-    constructor(width, height, cell = 0) {
+    public width: number;
+    public height: number;
+    public cells: Cell[];
+
+    constructor(width: number, height: number, cell: number = 0) {
         this.width = width;
         this.height = height;
-        this.cells = Array(width * height).fill(cell);
+        this.cells = Array(width*height).fill(cell);
     }
-    get(x, y) {
-        return this.cells[y * this.width + x];
+
+    get(x: number, y: number): Cell {
+        return this.cells[y*this.width + x];
     }
-    set(x, y, cell) {
-        return this.cells[y * this.width + x] = cell;
+
+    set(x: number, y: number, cell: Cell) {
+        return this.cells[y*this.width + x] = cell;
     }
 }
-function mod(a, b) {
-    return (a % b + b) % b;
+
+function mod(a: number, b: number): number {
+    return (a%b + b)%b
 }
-function countNbors(board, nbors, x0, y0) {
+
+function countNbors(board: Board, nbors: number[], x0: number, y0: number) {
     nbors.fill(0);
     for (let dy = -1; dy <= 1; ++dy) {
         for (let dx = -1; dx <= 1; ++dx) {
@@ -36,7 +36,18 @@ function countNbors(board, nbors, x0, y0) {
         }
     }
 }
-const Seeds = [
+
+interface State {
+    "color": string;
+    "default": number;
+    "transitions": {
+        [key: string]: number;
+    }
+}
+
+type Automaton = State[];
+
+const Seeds: Automaton = [
     {
         "transitions": {
             "62": 1,
@@ -50,7 +61,8 @@ const Seeds = [
         "color": "#FF5050",
     },
 ];
-const GoL = [
+
+const GoL: Automaton = [
     {
         "transitions": {
             "53": 1,
@@ -67,7 +79,8 @@ const GoL = [
         "color": "#FF5050",
     },
 ];
-const BB = [
+
+const BB: Automaton = [
     // 0 - Dead
     {
         "transitions": {
@@ -95,7 +108,8 @@ const BB = [
         "color": "#50FF50",
     }
 ];
-function computeNextBoard(automaton, current, next) {
+
+function computeNextBoard(automaton: Automaton, current: Board, next: Board) {
     console.assert(current.width == next.width);
     console.assert(current.height == next.height);
     const nbors = new Array(automaton.length).fill(0);
@@ -110,40 +124,46 @@ function computeNextBoard(automaton, current, next) {
         }
     }
 }
-function render(ctx, automaton, board) {
-    const CELL_WIDTH = ctx.canvas.width / board.width;
-    const CELL_HEIGHT = ctx.canvas.height / board.height;
+
+function render(ctx: CanvasRenderingContext2D, automaton: Automaton, board: Board) {
+    const CELL_WIDTH = ctx.canvas.width/board.width;
+    const CELL_HEIGHT = ctx.canvas.height/board.height;
+
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     for (let y = 0; y < board.height; ++y) {
         for (let x = 0; x < board.width; ++x) {
-            const rx = x * CELL_WIDTH;
-            const ry = y * CELL_HEIGHT;
+            const rx = x*CELL_WIDTH;
+            const ry = y*CELL_HEIGHT;
             ctx.fillStyle = automaton[board.get(x, y)].color;
             ctx.fillRect(rx, ry, CELL_WIDTH, CELL_HEIGHT);
         }
     }
 }
-function bytesAsHexString(bytes) {
+
+function bytesAsHexString(bytes: Uint8Array): string {
     let result = "";
     for (let i = 0; i < bytes.length; ++i) {
         result += bytes[i].toString(16).padStart(2, '0').toUpperCase();
     }
     return result;
 }
-function transcendentalApprehensionOfImage(image, rx, ry, rw, rh) {
+
+function transcendentalApprehensionOfImage(image: ImageData, rx: number, ry: number, rw: number, rh: number): [Board, Automaton] {
     const board = new Board(rw, rh);
     let count = 0;
-    const colorToState = {};
+    const colorToState: { [key: string]: number } = {};
     for (let y = ry; y < ry + rh; ++y) {
         for (let x = rx; x < rx + rw; ++x) {
-            const pixel = new Uint8Array(image.data.buffer, (y * image.width + x) * 4, 4);
+            const pixel = new Uint8Array(image.data.buffer, (y*image.width + x)*4, 4);
             const color = bytesAsHexString(pixel);
             if (colorToState[color] === undefined) {
                 colorToState[color] = count++;
             }
+
             board.set(x - rx, y - ry, colorToState[color]);
         }
     }
+
     const automaton = Object.keys(colorToState).map((color) => {
         return {
             "color": `#${color}`,
@@ -151,13 +171,17 @@ function transcendentalApprehensionOfImage(image, rx, ry, rw, rh) {
             "transitions": {},
         };
     });
+
     return [board, automaton];
 }
-window.onload = () => __awaiter(void 0, void 0, void 0, function* () {
-    const cute = yield stbi_load_from_url("img/Cute People Icon v2.png");
-    const [cuteBoard, cuteAutomaton] = transcendentalApprehensionOfImage(cute, 0, 0, cute.width / 4, cute.height / 3);
+
+window.onload = async () => {
+    const cute = await stbi_load_from_url("img/Cute People Icon v2.png");
+    const [cuteBoard, cuteAutomaton] = transcendentalApprehensionOfImage(cute, 0, 0, cute.width/4, cute.height/3);
+
     console.log(cuteBoard);
     console.log(cuteAutomaton);
+
     const nbors = new Array(cuteAutomaton.length).fill(0);
     for (let y = 0; y < cuteBoard.height; ++y) {
         for (let x = 0; x < cuteBoard.width; ++x) {
@@ -169,9 +193,11 @@ window.onload = () => __awaiter(void 0, void 0, void 0, function* () {
             }
         }
     }
+
     console.log(cuteAutomaton);
+
     const canvasId = "app";
-    const app = document.getElementById(canvasId);
+    const app = document.getElementById(canvasId) as HTMLCanvasElement;
     if (app === null) {
         throw new Error(`Could not find canvas ${canvasId}`);
     }
@@ -180,56 +206,66 @@ window.onload = () => __awaiter(void 0, void 0, void 0, function* () {
     if (ctx === null) {
         throw new Error(`Could not initialize 2d context`);
     }
+
     const nextId = "next";
-    const next = document.getElementById(nextId);
+    const next = document.getElementById(nextId) as HTMLButtonElement;
     if (next == null) {
         throw new Error(`Could not find button ${nextId}`);
     }
+
     const playId = "play";
-    const play = document.getElementById(playId);
+    const play = document.getElementById(playId) as HTMLButtonElement;
     if (play == null) {
         throw new Error(`Could not find button ${playId}`);
     }
+
     let currentAutomaton = cuteAutomaton;
-    let currentBoard = cuteBoard;
-    let nextBoard = new Board(currentBoard.width, currentBoard.height);
-    app.height = app.width * (currentBoard.height / currentBoard.width);
+    let currentBoard: Board = cuteBoard;
+    let nextBoard: Board = new Board(currentBoard.width, currentBoard.height);
+
+    app.height = app.width*(currentBoard.height/currentBoard.width);
+    
     app.addEventListener("click", (e) => {
-        const CELL_WIDTH = app.width / currentBoard.width;
-        const CELL_HEIGHT = app.height / currentBoard.height;
-        const x = Math.floor(e.offsetX / CELL_WIDTH);
-        const y = Math.floor(e.offsetY / CELL_HEIGHT);
+        const CELL_WIDTH = app.width/currentBoard.width;
+        const CELL_HEIGHT = app.height/currentBoard.height;
+        
+        const x = Math.floor(e.offsetX/CELL_WIDTH);
+        const y = Math.floor(e.offsetY/CELL_HEIGHT);
+
         const state = document.getElementsByName("state");
         for (let i = 0; i < state.length; ++i) {
-            if (state[i].checked) {
+            if ((state[i] as HTMLInputElement).checked) {
                 currentBoard.set(x, y, i);
                 render(ctx, currentAutomaton, currentBoard);
                 return;
             }
         }
     });
+
     const nextState = () => {
         computeNextBoard(currentAutomaton, currentBoard, nextBoard);
         [currentBoard, nextBoard] = [nextBoard, currentBoard];
         render(ctx, currentAutomaton, currentBoard);
     };
+
     next.addEventListener("click", nextState);
+
     const PLAY_PERIOD = 100;
-    let playInterval = setInterval(nextState, PLAY_PERIOD);
+    let playInterval: number | null = setInterval(nextState, PLAY_PERIOD);
+
     play.addEventListener("click", () => {
         if (playInterval === null) {
             playInterval = setInterval(nextState, PLAY_PERIOD);
             play.innerText = "Pause";
-        }
-        else {
+        } else {
             clearInterval(playInterval);
             playInterval = null;
             play.innerText = "Play";
         }
     });
+
     render(ctx, currentAutomaton, currentBoard);
-});
-// TODO: pause
+};
+
 // TODO: drawing the cells
 // TODO: autopopulate radio buttons based on the current automaton
-//# sourceMappingURL=index.js.map
